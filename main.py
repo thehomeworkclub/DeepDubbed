1e5f3652a7cd003bd00c69174823f79921270d20
primboi <81822069+primitt@users.noreply.github.com>
2023-11-11 15:48:07 -0800

    bing chillong

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from elevenlabs import generate, stream, set_api_key
 import os
-
+from yt_dlp import YoutubeDL
+ydl_opts = {
+    'outtmpl': '%(title)s.%(ext)s',
+    'quiet': 'true',
+    'format': 'bestvideo+bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4',
+    }],
+}
+def download():
+    global ydl_opts
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([str(input("Enter URL: "))])
+download()
 set_api_key(os.environ['ELEVENLABS_API_KEY'])
